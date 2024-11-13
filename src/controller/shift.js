088dd61491b129b3fed0d8f1802e601d0ff234a7
Bassel Ahmed ABO-Khabsa <baakswaiengineer@gmail.com>
2024-11-13 16:45:00 +0300

    enhance code

--- src/controller/shift.js
+++ src/controller/shift.js
@@ -206,17 +206,13 @@ const fetchShifts = catchAsyncErrors(async (req, res, next) => {
 
   const where = {};
 
-  if (workerName) {
-    where["$worker.fullName$"] = { [Op.eq]: workerName };
-  }
+  if (workerName)
+    where["$worker.fullName$"] = { [Op.like]: `%${workerName.trim()}%` };
 
-  if (workerPhone) {
-    where["$worker.phone$"] = { [Op.like]: `${workerPhone}` };
-  }
+  if (workerPhone) where["$worker.phone$"] = { [Op.like]: `%${workerPhone}%` };
 
-  if (companyName) {
-    where["$company.name$"] = { [Op.like]: `${companyName}` };
-  }
+  if (companyName)
+    where["$company.name$"] = { [Op.like]: `%${companyName.trim()}%` };
 
   if (date1 && date2) {
     const startDate = new Date(date1);
