58851f230be29d4e725e1ab55910195efcba2773
Bassel Ahmed ABO-Khabsa <baakswaiengineer@gmail.com>
2024-11-25 20:39:04 +0300

    edited language & logo

--- src/controller/report.js
+++ src/controller/report.js
@@ -1,18 +1,8 @@
 const { catchAsyncErrors } = require("../middleware/catchAsyncErrors");
-const { ErrorHandler } = require("../middleware/errorMiddleware");
 const { Shift, User, Company } = require("../models");
 const PDFDocument = require("pdfkit");
 const { Op } = require("sequelize");
 const path = require("path");
-
-const hebrewFontPathRegular = path.join(
-  __dirname,
-  "../assets/NotoSansHebrew_SemiCondensed-Regular.ttf"
-);
-const hebrewFontPathBold = path.join(
-  __dirname,
-  "../assets/NotoSansHebrew_SemiCondensed-Bold.ttf"
-);
 
 const calculateTimeDifference = (startHour, endHour) => {
   if (!startHour || !endHour) return "00:00";
@@ -34,8 +24,6 @@ const calculateTimeDifference = (startHour, endHour) => {
 
   return `${String(diffH).padStart(2, "0")}:${String(diffM).padStart(2, "00")}`;
 };
-
-const reverseHebrewText = (text) => text.split(" ").reverse().join("  ");
 
 const buildReport = catchAsyncErrors(async (req, res, next) => {
   const { workerName, workerPhone, companyName, date1, date2, searcher } =
@@ -78,7 +66,7 @@ const buildReport = catchAsyncErrors(async (req, res, next) => {
       {
         model: User,
         as: "worker",
-        attributes: ["phone", "id", "fullName", "personal_id"], // Include necessary worker details
+        attributes: ["phone", "id", "fullName", "personal_id"],
         required: false,
       },
       {
@@ -107,7 +95,6 @@ const buildReport = catchAsyncErrors(async (req, res, next) => {
     });
   }
 
-  // Extract worker details from the first shift
   const worker = shifts[0].worker;
 
   const doc = new PDFDocument();
@@ -125,39 +112,34 @@ const buildReport = catchAsyncErrors(async (req, res, next) => {
     res.send(pdfBuffer);
   });
 
-  const logoPath = path.join(__dirname, "../assets/logo.jpg");
-
-  doc.font(hebrewFontPathRegular);
+  const logoPath = path.join(__dirname, "../assets/logo.png");
 
   let totalHours = 0;
   let currentPage = 1;
 
   const addHeader = () => {
-    doc.image(logoPath, 50, 40, { width: 250 });
-
     doc
       .fontSize(12)
-      .text(reverseHebrewText(worker.fullName) || "-", 380, 50, {
-        align: "right",
-      })
-      .text(worker.phone || "-", 380, 70, { align: "right" });
-    if (worker.personal_id)
-      doc.text(worker.personal_id, 380, 90, { align: "right" });
+      .text(worker.fullName || "-", 50, 50)
+      .text(worker.phone || "-", 50, 70);
+    if (worker.personal_id) doc.text(worker.personal_id, 50, 90);
+
+    const currentDate = new Date().toLocaleDateString();
+    doc.fontSize(14).text(currentDate, 50, 110);
+
+    doc.image(logoPath, 320, 40, { width: 250 });
 
     doc.lineWidth(2);
     doc.moveTo(50, 125).lineTo(555, 125).stroke();
 
-    const currentDate = new Date().toLocaleDateString();
-    doc.fontSize(14).text(currentDate, 380, 110, { align: "right" });
-
     doc
       .fontSize(10)
-      .text(reverseHebrewText(`סה"כ שעות`), 90, 135)
-      .text(reverseHebrewText("תאריך"), 170, 135)
-      .text(reverseHebrewText("שעת סיום"), 240, 135)
-      .text(reverseHebrewText("שעת התחלה"), 325, 135)
-      .text(reverseHebrewText("סוג עבודה"), 400, 135)
-      .text(reverseHebrewText("שם החברה"), 485, 135)
+      .text("Company Name", 65, 135)
+      .text("Work Type", 170, 135)
+      .text("Start Time", 240, 135)
+      .text("End Time", 325, 135)
+      .text("Date", 400, 135)
+      .text("Total Hours", 485, 135);
   };
 
   const addFooter = () => {
@@ -167,49 +149,46 @@ const buildReport = catchAsyncErrors(async (req, res, next) => {
 
   addHeader();
 
-  let yPosition = 156; // Position just below the header
+  let yPosition = 156;
   shifts.forEach((shift) => {
     const hoursWorked = calculateTimeDifference(shift.startHour, shift.endHour);
 
-    // Check if we need to start a new page
     if (yPosition > 680) {
-      addFooter(); // Add footer before moving to new page
-      doc.addPage(); // Create a new page
-      addHeader(); // Add header to the new page
-      yPosition = 156; // Reset yPosition just below the header
+      addFooter();
+      doc.addPage();
+      addHeader();
+      yPosition = 156;
     }
 
-    // Draw a background rectangle for better row separation
-    doc.rect(50, yPosition - 5, 505, 20).fill("#f9f9f9").stroke();
+    doc
+      .rect(50, yPosition - 5, 505, 20)
+      .fill("#f9f9f9")
+      .stroke();
 
-    // Shift details in the reversed column order
-    doc.fillColor("black")
-      .text(hoursWorked, 90, yPosition)
-      .text(
-        shift.date ? new Date(shift.date).toLocaleDateString() : "N/A",
-        170,
-        yPosition,
-      );
-    shift.endHour
-      ? doc.text(shift.endHour.slice(0, 5), 240, yPosition)
-      : doc.text("N/A", 240, yPosition);
+    doc.fillColor("black").text(hoursWorked, 485, yPosition);
+
+    doc.text(
+      shift.date ? new Date(shift.date).toLocaleDateString() : "N/A",
+      400,
+      yPosition
+    );
+
     shift.startHour
-      ? doc.text(shift.startHour.slice(0, 5), 325, yPosition, {
-          align: "right",
-        })
+      ? doc.text(shift.startHour.slice(0, 5), 240, yPosition)
+      : doc.text("N/A", 240, yPosition);
+    shift.endHour
+      ? doc.text(shift.endHour.slice(0, 5), 325, yPosition)
       : doc.text("N/A", 325, yPosition);
-    doc.text(shift.workType || "-", 400, yPosition);
-    doc.text(shift.company.name, 485, yPosition);
 
-    // Update total hours
+    doc.text(shift.workType || "-", 170, yPosition);
+    doc.text(shift.company.name, 65, yPosition);
+
     const [hours, minutes] = hoursWorked.split(":").map(Number);
     totalHours += hours + minutes / 60;
 
-    // Move down for the next row
     yPosition += 25;
   });
 
-  // Final total hours calculation
   const totalHoursString = `${Math.floor(totalHours)
     .toString()
     .padStart(2, "0")}:${String(Math.round((totalHours % 1) * 60)).padStart(
@@ -217,7 +196,6 @@ const buildReport = catchAsyncErrors(async (req, res, next) => {
     "0"
   )}`;
 
-  // Print total hours on the last page
   if (yPosition > 680) {
     addFooter();
     doc.addPage();
@@ -225,17 +203,7 @@ const buildReport = catchAsyncErrors(async (req, res, next) => {
     yPosition = 156;
   }
 
-  doc
-    .font(hebrewFontPathBold)
-    .fontSize(12)
-    .text(
-      reverseHebrewText(`סה"כ שעות: ${totalHoursString}`),
-      90,
-      yPosition + 20,
-      {
-        align: "left",
-      }
-    );
+  doc.fontSize(12).text(`Total Hours: ${totalHoursString}`, 50, yPosition + 20);
 
   doc.end();
 });
